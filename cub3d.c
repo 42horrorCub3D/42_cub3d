dd5b21aee7d531892303cd628fc607c51a6ac6b6
Joo-hyun-Kang <autoba9687@gmail.com>
2022-12-29 08:17:17 +0900

    chore: 불 필요한 주석 제거

--- cub3d.c
+++ cub3d.c
@@ -32,19 +32,10 @@ int main(int argc, char **argv) {
         exit_with_error("Error\nFail to open file!\n");
     init_game(&game, fd);
     check_map_parsing(&game);
-    // draw(&game);
 
     game.mlx_win = mlx_new_window(game.mlx_ptr, WIDTH, HEIGHT, "test");
     mlx_loop_hook(game.mlx_ptr, &make_view, &game);
 	mlx_hook(game.mlx_win, X_EVENT_KEY_RELEASE, 0, &key_press, &game);
 	mlx_hook(game.mlx_win, X_EVENT_KEY_EXIT, 0, &finish_window, &game);
     mlx_loop(game.mlx_ptr);
-    
-    /* 맵 잘 찍히나 확인좀 */
-    /*
-    t_node *node = game.map->head->next;
-    while (node != game.map->tail) {
-        printf("%s\n", node->line);
-        node = node->next;
-    } */
 }
