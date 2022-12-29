176ad553d106ae934d512c16baa50c06ce62249a
Joo-hyun-Kang <autoba9687@gmail.com>
2022-12-29 11:57:13 +0900

    chore: struct.h 코딩 표준에 맞게 수정

--- header/struct.h
+++ header/struct.h
@@ -23,80 +23,79 @@
 # define DEGREE                 (M_PI)
 
 enum e_DIR {
-    BUF = 0,
-    NO = 1,
-    WE = 2,
-    EA = 3,
-    SO = 4,
-    DIR_CNT = 5,
+	BUF = 0,
+	NO = 1,
+	WE = 2,
+	EA = 3,
+	SO = 4,
+	DIR_CNT = 5,
 };
 
 typedef struct s_node
 {
-    struct s_node  *next;
-    struct s_node  *prev;
-    char    *line;
-    int     col;
-}   t_node;
+	struct	s_node *next;
+	struct	s_node *prev;
+	char	*line;
+	int		col;
+}	t_node;
 
 typedef struct s_tmp
 {
-    t_node  *head;
-    t_node  *tail;
-    int     max_col;
-    int     row;
+	t_node	*head;
+	t_node	*tail;
+	int		max_col;
+	int		row;
 }	t_tmp;
 
 typedef struct s_vec
 {
-    double  p_x; /* player x, y */
-    double  p_y;
-    double  d_x; /* direction x, y */
-    double  d_y;
-    double  w_x; /* plane(window) x, y */
-    double  w_y;
-} t_vec;
+	double	p_x;
+	double	p_y;
+	double	d_x;
+	double	d_y;
+	double	w_x;
+	double	w_y;
+}	t_vec;
 
 typedef struct s_ray
 {
-    double cam; /* cam horizontal */
-    double ray_x; /* 광선의 방향벡터 */
-    double ray_y;
-    int map_x;
-    int map_y;
-    double side_x;
-    double side_y;
-    double delta_x;
-    double delta_y;
-    int step_x;
-    int step_y;
-    int hit;
-    int side;
-    double wall_d;
-} t_ray;
+	double	cam;
+	double	ray_x;
+	double	ray_y;
+	int		map_x;
+	int		map_y;
+	double	side_x;
+	double	side_y;
+	double	delta_x;
+	double	delta_y;
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side;
+	double	wall_d;
+}	t_ray;
 
 typedef struct s_image
 {
-    void*       img_ptr;
-    int*        img_data;
-    int         bits;
-    int         line_size;
-    int         endien;
-} t_image;
-
+	void	*img_ptr;
+	int		*img_data;
+	int		bits;
+	int		line_size;
+	int		endien;
+}	t_image;
 
 typedef struct s_game
 {
-    char        *map;
-    int         row;
-    int         col;
-    t_vec       *vec;
-    void        *mlx_ptr;
-    void        *mlx_win;
-    t_image     image[DIR_CNT];
-    int    floor;
-    int    ceil;
-    void		*win;
-} t_game;
+	char		*map;
+	int			row;
+	int			col;
+	t_vec		*vec;
+	void		*mlx_ptr;
+	void		*mlx_win;
+	t_image		image[DIR_CNT];
+	int			floor;
+	int			ceil;
+	void		*win;
+}	t_game;
 
 #endif
