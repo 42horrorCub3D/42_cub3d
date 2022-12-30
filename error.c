279b6f4269be22e64a87df3426da983bbd0da504
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-30 19:32:05 +0900

    fix : norm check - error.c

--- error.c
+++ error.c
@@ -1,8 +1,7 @@
-#include "header/cub3d.h"
-#include <stdio.h>
-#include <string.h>
+#include "header/cub3d.h">
 
-void exit_with_error(const char *msg) {
-    write(1, msg, ft_strlen(msg));
-    exit(SUCCESS);
+void	exit_with_error(const char *msg)
+{
+	write(1, msg, ft_strlen(msg));
+	exit(SUCCESS);
 }
