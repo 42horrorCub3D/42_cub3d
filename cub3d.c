a7f12522986df5feeae71c884922c8ec1b883582
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-30 19:31:05 +0900

    fix : norm check - cub3d.c more check

--- cub3d.c
+++ cub3d.c
@@ -5,6 +5,7 @@ int	finish_window(t_game *game)
 	mlx_destroy_window(game->mlx_ptr, game->mlx_win);
 	exit(1);
 }
+
 /*
 void	check_map_parsing(t_game *game)
 {
@@ -22,6 +23,9 @@ void	check_map_parsing(t_game *game)
 */
 void	set_images(t_game *game)
 {
+	/* 
+	줄 너무 길어서 변수 수정 필요 
+	*/
 	game->image[BUF].img_ptr = mlx_new_image(game->mlx_ptr, WIDTH, HEIGHT);
 	game->image[BUF].img_data = (int *)mlx_get_data_addr(game->image[BUF].img_ptr, &game->image[BUF].bits, &game->image[BUF].line_size, &game->image[BUF].endien);
 	game->image[NO].img_data = (int *)mlx_get_data_addr(game->image[NO].img_ptr, &game->image[NO].bits, &game->image[NO].line_size, &game->image[NO].endien);
@@ -41,7 +45,9 @@ int	main(int argc, char **argv)
 	if (fd == FAIL)
 		exit_with_error("Error\nFail to open file!\n");
 	init_game(&game, fd);
+	/*
 	check_map_parsing(&game);
+	*/
 	set_images(&game);
 	game.mlx_win = mlx_new_window(game.mlx_ptr, WIDTH, HEIGHT, "test");
 	mlx_loop_hook(game.mlx_ptr, &make_view, &game);
